941c9ede4014344985df07f4900e4ef9034cd68c
Ojas Innovative Technologies Pvt Ltd <ojas.bitbucket@gmail.com>
2020-01-30 14:47:00 +0530

    Initial Commit

---
+++ Bandana_AI_Eng_IOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bandana_AI_Eng_IOS
+//
+//  Created by Swaminath Kosetty on 30/01/20.
+//  Copyright © 2020 Ojas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Bandana_AI_Eng_IOSTests/Bandana_AI_Eng_IOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  Bandana_AI_Eng_IOSTests.swift
+//  Bandana_AI_Eng_IOSTests
+//
+//  Created by Swaminath Kosetty on 30/01/20.
+//  Copyright © 2020 Ojas. All rights reserved.
+//
+
+import XCTest
+@testable import Bandana_AI_Eng_IOS
+
+class Bandana_AI_Eng_IOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
